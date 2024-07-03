7b640e715f8a78726cf67f6703e67f92df3c1011
Arnab Golder <arnabgolder7@gmail.com>
2024-07-03 10:18:31 +0530

    handling body params & query

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('date', views.display_date, name='display_date'),
+    path('showform/', views.showform, name="showform"),
+    path("showform/getform/", views.getform, name='getform'),
+    path('dishes/<str:dish>', views.menuitems, name='dishes'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,9 +5,42 @@ from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome to little Lemon website')
+    path = request.path
+    scheme = request.scheme
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+    msg = f"""<br>
+    <br> Path: {path}
+    <br> Address: {address}
+    <br> Scheme: {scheme}
+    <br> Method: {method}
+    <br> User_agent: {user_agent}
+    <br> Path info : {path_info}
+    """
+    return HttpResponse(msg, content_type='text/html', charset='utf-8')
 
 
 def display_date(request):
     date_val = datetime.now().year
     return HttpResponse(date_val)
+
+def getform(request):
+    if request.method == "POST":
+        id=request.POST['id']
+        name=request.POST['name']
+    return HttpResponse("Name:{} UserID:{}".format(name, id))
+def showform(request):
+    return render(request, "form.html")
+
+
+
+def menuitems(request, dish):
+    items = {
+        'pasta': 'the test files here are',
+        'falafal': 'the middle east dish',
+        'cheesecake': 'milk items here are',
+    }
+    desc = items[dish]
+    return HttpResponse(f'<h2>{dish}</h2>' + desc)
